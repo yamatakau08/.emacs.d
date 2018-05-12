551518532e2acad22a4dcac3d362597ebbe44a2d
yamatakau08 <yamatakau08@gmail.com>
2018-05-12 11:14:37 +0900

    enable ricty font in font-setting.el add comment on require part in .search-web.el

--- .search-web.el
+++ .search-web.el
@@ -1,5 +1,5 @@
 ;(add-to-list 'load-path "~/.emacs.d/elpa/search-web-20150312.403")
-(require 'search-web)
+(require 'search-web) ; need require
 
 ;- search-web-default-browser
 ;- search-web-in-emacs-browser
--- font-setting.el
+++ font-setting.el
@@ -1,3 +1,8 @@
 ;;; font
-;(add-to-list 'default-frame-alist '(font . "ricty-12"))
+
+;; ricty
+;; refer the folloing page install on mac
+;; https://hajipro.com/local-development-environment-mac/ricty-diminished
+;; ricty diminished でも同じ設定で使える
+(add-to-list 'default-frame-alist '(font . "ricty-14"))
 ;(add-to-list 'default-frame-alist '(font . "Menlo-16"))
