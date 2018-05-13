e727980c54fd0ab55234d96b1042d6a8b9d2f950
yamatakau08 <yamatakau08@gmail.com>
2018-05-13 23:46:11 +0900

    support darwin environment

--- .dired.el
+++ .dired.el
@@ -1,9 +1,6 @@
-;(let ((fname "2015法政大学_T_英語解答.pdf"))
-;  (message "open %s" fname))
-;"open 2015法政大学_T_英語解答.pdf"
-
 ; dired にて、windows に関連付けられたファイルを起動する。
-; http://uenox.ld.infoseek.co.jp/elisp/index.html
+; http://www.bookshelf.jp/soft/meadow_25.html#SEC282
+; http://uenox.ld.infoseek.co.jp/elisp/index.html (site disappear)
 (defun uenox-dired-winstart ()
   "Type '\\[uenox-dired-winstart]': win-start the current line's file."
   (interactive)
@@ -13,8 +10,8 @@
 	 ((eq system-type 'windows-nt)
 	  (w32-shell-execute "open" fname))
 	 ((eq system-type 'darwin)
-	  (shell-command-to-string "/usr/bin/open ~/tmp/auお客さまサポート.pdf")))
-	(message "win-started %s" fname))))
+	  (shell-command-to-string (format "open %s" fname))))
+	(message "open %s" fname))))
 
 (add-hook 'dired-mode-hook
 	  (lambda ()
