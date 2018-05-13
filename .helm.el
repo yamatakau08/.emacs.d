1942c54a36a1ff287dc75a477a33b62a23e23484
yamatakau08 <yamatakau08@gmail.com>
2018-05-13 22:52:18 +0900

    add global-set-key setting in .helm.el

--- .helm.el
+++ .helm.el
@@ -1,4 +1,8 @@
 ;(require 'helm-config) ; https://github.com/emacs-helm/helm/wiki#install
 (require 'helm) ; helm-config を require すると、helm-migiemo-mode を設定するとエラーになるので、(require 'helm) する
 
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x f") 'helm-find-files)
+
 (helm-migemo-mode 1)
+
