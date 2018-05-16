aa501a388d119eacb2c1fcc4f90326ef2095e30e
yamatakau08 <yamatakau08@gmail.com>
2018-05-16 18:05:38 +0900

    .

---
+++ .wl.el
@@ -0,0 +1,25 @@
+;; for imap connection
+(cond 
+  ((equal ssl-program-name "openssl")
+   (setq elmo-imap4-default-port 993) ; imap over ssl
+   (setq ssl-program-arguments '("s_client" "-quiet" "-host" host "-port" service))
+   )
+  ((equal ssl-program-name "gnutls-cli")
+   (setq elmo-imap4-default-port 993)
+;  (setq ssl-program-arguments '("-p" service host))
+   (setq ssl-program-arguments '("--insecure" "-p" service host))
+   )
+  )
+
+;; for summary
+(setq wl-summary-line-format "%n%T%P %Y/%M/%D(%W)%h:%m %t%[%17(%c %f%) %] %s") ; %Y 年追加
+
+; サマリーモードに入った際に、日付逆順でソート
+(defun my-wl-summary-mode-hook ()
+  (interactive)
+  (wl-summary-sort-by-date t))
+; http://www.ss.scphys.kyoto-u.ac.jp/person/yasui/emacs/mail.html
+; より、サマリモードに入った直後のhookにする事で、正常動作
+(add-hook  'wl-summary-prepared-hook 'my-wl-summary-mode-hook) 
+;(add-hook 'wl-summary-mode-hook     'my-wl-summary-mode-hook)
+
