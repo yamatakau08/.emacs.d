58a9f15877d3d8701d32635c714510c06103b42e
yamatakau08 <yamatakau08@gmail.com>
2018-05-17 18:56:28 +0900

    update summary_mode_prepared_hook to cursor to to in summary buffer

--- .migemo.el
+++ .migemo.el
@@ -3,17 +3,18 @@
 (cond
  ((eq system-type 'windows-nt)
   ;; https://nagayasu-shinya.com/emacs-cmigemo-windows/
-  ;; の説明にあるような環境変数等設定しなくても動く
+  ;; の説明にあるような環境変数等を設定しなくても動く
   ;; http://grugrut.hatenablog.jp/entry/2015/04/emacs-migemo-on-windows
-  ;; "正しい設定" より、windows環境の外部コマンドを使うような場合には、絶対パスで記述するほうがよいようだ。
-  ;(setq migemo-command "C:/yama/bin/cmigemo-default-win64/cmigemo.exe")
-  (setq migemo-command "C:/yama/bin/cmigemo-default-win64/cmigemo") ; file suffixの.exeを指定しなくても動作する
-
-  (setq migemo-command "~/bin/cmigemo-default-win64/cmigemo") ; 相対パスでも動作する
+  ;; の"正しい設定" より、windows環境の外部コマンドを使うような場合には、絶対パスで記述する方がよいようだ。
+  ;; が、相対パスでも動作する
+  ;; file suffix .exeを指定しなくても動作するが、.exe を付けておくと、
+  ;; file-exist-p 等での存在確認時には、.exe がないと nil になってしまう
+  (setq migemo-command "C:/yama/bin/cmigemo-default-win64/cmigemo.exe")
+  (setq migemo-command "~/bin/cmigemo-default-win64/cmigemo.exe") ; 相対パスでも動作する
 
   (setq migemo-options '("-q" "--emacs" "-i" "\a")) ;cygwin ruby では動作せず 調査必要!
 
-  ;; windows環境では、cp932-unix,utf-8-unixどちらでも動く
+  ;; windows環境では、cp932-unix,utf-8-unixどちらでも動く→utf-8-unixでしか動作しなくなった？ 
   ;; cp932-unix
   ;(setq migemo-coding-system 'cp932-unix)
   ;(setq migemo-dictionary "C:/yama/.emacs.d/conf/migemo/dict/cp932/migemo-dict")
@@ -34,7 +35,8 @@
 (load-library "migemo")
 (migemo-init)
 
-(unless (file-exists-p migemo-command)
-  (setq migemo-isearch-enable-p nil) ; disable migemo when type C-s
+(unless (file-exists-p migemo-command) ; migemo-command が存在しない場合
+  (message "%s is not found!" migemo-command)
+  (setq migemo-isearch-enable-p nil) ; disable migemo
 )
 
