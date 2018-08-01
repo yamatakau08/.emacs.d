6f6372c1b836b659fbd45e25af86a5c2402aadfd
yamatakau08 <yamatakau08@gmail.com>
2018-08-01 23:45:59 +0900

    add the defadvice for magit-clone. it's for testing and doesn't work! need to study

--- .magit.el
+++ .magit.el
@@ -1,1 +1,6 @@
-;
+;; following code doesn't work
+(defadvice magit-clone (arround magit-clone-repository-template-windows)
+  "set the \"git@github:Username/Repository_name.git\" in prompt"
+  (let ((ad-set-arg 0 "git@github:Username/Repository_name.git"))
+    ad-do-it))
+(ad-activate 'magit-clone)
