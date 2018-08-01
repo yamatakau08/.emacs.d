1e5d73724bc627ac3cd6824fdf57b878dfd988ae
yamatakau08 <yamatakau08@gmail.com>
2018-08-02 00:05:06 +0900

    fix the spell miss arround to around

--- .magit.el
+++ .magit.el
@@ -1,5 +1,5 @@
 ;; following code doesn't work
-(defadvice magit-clone (arround magit-clone-repository-template-windows)
+(defadvice magit-clone (around magit-clone-repository-template-windows)
   "set the \"git@github:Username/Repository_name.git\" in prompt"
   (let ((ad-set-arg 0 "git@github:Username/Repository_name.git"))
     ad-do-it))
