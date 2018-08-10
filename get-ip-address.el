3e1c3177b2330b00d56d099429c57c03cb03b73d
yamatakau08 <yamatakau08@gmail.com>
2018-08-10 17:59:40 +0900

    https://gist.github.com/tkhoa2711/ef99938c8752ca3e52c2 and add my function

---
+++ get-ip-address.el
@@ -0,0 +1,32 @@
+(defun get-ip-address (&optional dev)
+  "Get the IP-address for device DEV (default: eth0) of the current machine."
+  (let ((dev (if dev dev "eth0")))
+    (format-network-address (car (network-interface-info dev)) t)))
+
+;(defun get-ip-address-first-octet (&optional dev)
+;  (let ((dev (if dev dev "eth0")))
+;    (car (split-string (get-ip-address dev) "\\."))))
+
+(defun get-ip-address-first-octet (&optional dev)
+  (let* ((dev (if dev dev "eth0"))
+	 (ip-address (get-ip-address dev)))
+    (if ip-address
+	(car (split-string ip-address "\\."))
+      nil)))
+    
+(defun private-network-p (first-octet)
+  (if (string= first-octet "192") t nil))
+
+(defun check-private-network ()
+  (cond 
+   ((eq system-type 'windows-nt)
+    (private-network-p (get-ip-address-first-octet "eth0")))
+   ((eq system-type 'darwin) 
+    (private-network-p (get-ip-address-first-octet "en0")))
+   ((eq system-type 'gnu/linux) ; Linux Mint
+    (private-network-p (get-ip-address-first-octet "en0")))
+   ))
+
+
+
+  
