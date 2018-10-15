59a375aab88b14c947b3d6b546d359e5d264b0a5
yamatakau08 <yamatakau08@gmail.com>
2018-10-15 18:31:16 +0900

    add remove "Validate" link at the bottom of exported html

--- .org-mode.el
+++ .org-mode.el
@@ -1,1 +1,5 @@
 (setq org-edit-src-content-indentation 0) ; ソースブロックの中身が右にずらさないように左端にする
+
+;;; to remove "Validate" link at the bottom of exported html
+;;; https://stackoverflow.com/questions/15134911/in-org-mode-how-do-i-remove-the-validate-xhtml-1-0-message-from-html-export
+(setq org-html-validation-link nil)
