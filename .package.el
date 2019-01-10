fe31b7fa7e7a93fe95cba60c11a2b4d65b4699cd
Takahiro Yamaguchi <yama@jp.sony.com>
2019-01-10 19:12:36 +0900

    url https from http and add url-proxy-services

--- .package.el
+++ .package.el
@@ -6,10 +6,32 @@
 ;(unless package--initialized 
 ;  (package-initialize t)) ; Unnecessary call on Emacs 27
 
+;;; to avoid the following error message in company network on Emacs 27.0
+;;; in case, M-x pakage-refresh-contents
+;;; https://emacs-jp.slack.com/archives/C1B5WTJLQ/p1547097797720600
+;;; Importing package-keyring.gpg...done
+;;; error in process sentinel: if: Error retrieving: http://melpa.org/packages/archive-contents (error connection-failed "failed with code 110" :host "melpa.org" :service 80)
+(unless (check-private-network)
+  (let ((proxy-server-port (format "%s:%s" wl-proxy-server wl-proxy-port)))
+    (setq url-proxy-services
+	  `(("http"  . ,proxy-server-port)
+	    ("https" . ,proxy-server-port)))))
+
+;(setq url-proxy-services
+;      '(("http"  . "proxy.kanto.sony.co.jp:10080")
+;        ("https" . "proxy.kanto.sony.co.jp:10080")))
+;;; in case, M-x list-packages with the above settings
+;;; https://emacs-jp.slack.com/archives/C1B5WTJLQ/p1547100014738900
+;;; Error while verifying signature archive-contents.sig:
+;;;
+;;; Command output:
+;;; gpg: no valid OpenPGP data found.
+(setq package-check-signature nil)
+
 ;;; defaultでは、("gnu" . "http://elpa.gnu.org/packages/") しかなかったので、以下を追加 
 (add-to-list 'package-archives
- '("melpa" . "http://melpa.org/packages/")
- '("org"   . "http://orgmode.org/elpa/"))
+ '("melpa" . "https://melpa.org/packages/")
+ '("org"   . "https://orgmode.org/elpa/"))
 
 ;; no effect! need to study
 ;; avoid to write custom-set-variables setting automatically in init.el
