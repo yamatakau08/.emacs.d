df5dba67ef295da74ec603e7864e9af8f535246c
yamatakau08 <yamatakau08@gmail.com>
2019-01-11 15:46:30 +0900

    enable org-mode in case of howm-mode

--- .org-mode.el
+++ .org-mode.el
@@ -4,3 +4,8 @@
 ;;; https://stackoverflow.com/questions/15134911/in-org-mode-how-do-i-remove-the-validate-xhtml-1-0-message-from-html-export
 (setq org-html-validation-link nil)
 
+;;; http://nobunaga.hatenablog.jp/entry/2015/10/25/161305
+;;; より howmモード時に、org-modeを有効にする
+;;; auto-mode-alist の設定だけで一旦対応
+(add-to-list 'auto-mode-alist '("\\.howm$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.txt$"  . org-mode))
