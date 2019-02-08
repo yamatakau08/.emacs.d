4fffecc13d0863fe48cf22873dcd4ae84831b390
yamatakau08 <yamatakau08@gmail.com>
2019-02-08 11:49:35 +0900

    change search-web-engin to "eijiro" to use eiwa-dictionary

--- .search-web.el
+++ .search-web.el
@@ -7,4 +7,18 @@
 
 ; http://kawamuray.hatenablog.com/entry/2013/11/03/180543
 ; http://no-maddojp.hatenablog.com/entry/2015/01/23/235238
-(setq search-web-engine "google ja")
+;(setq search-web-engine "google ja")
+(setq search-web-engine "eijiro")
+
+(defadvice w3m-browse-url (around w3m-browse-url-popwin activate)
+   (save-window-excursion ad-do-it)
+   (unless (get-buffer-window "*w3m*")
+      (pop-to-buffer "*w3m*")))
+
+(defadvice eww-render (around eww-render-popwin activate)
+  (save-window-excursion ad-do-it)
+  (unless (get-buffer-window "*eww*")
+    (pop-to-buffer "*eww*")))
+
+;(push "*eww*" popwin:special-display-config)
+;(push "*w3m*" popwin:special-display-config)
