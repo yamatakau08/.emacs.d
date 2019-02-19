6550baafe2b54e8a80e344027bceb1d74c492b72
yamatakau08 <yamatakau08@gmail.com>
2019-02-19 15:42:17 +0900

    delete org-gnus from org-modules

--- .org-mode.el
+++ .org-mode.el
@@ -18,3 +18,11 @@
 ;;; DONEの時刻を記録 
 ;;; TODOをC-c C-tでDONEした際に、CLOSED: [YYYY-MM-DD...] が追加される
 ;(setq org-log-done 'time) ; C-c C-x C-i(IN)後、C-c C-tで、DONEした際に、:LOGBOOK:.. END内に終了時間が自動でつくので不要 
+
+;;; 2019/02/19
+;;; To suppress Emacs is too slow when opening org-mode file or enable org-mode
+;;; on org-plus-contrib-20190218
+;;; because org-mode loads org-gnus even though I don't use gnus.... makes movemail
+;;; emacs-jp slack teach me how to not to load org-gnus
+(with-eval-after-load "org"
+  (delq 'org-gnus org-modules))
