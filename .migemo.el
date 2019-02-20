0f37fb6041042dabd5f00af607997a84253d3122
yamatakau08 <yamatakau08@gmail.com>
2019-02-20 15:15:00 +0900

    support fully cygwin environment.

--- .migemo.el
+++ .migemo.el
@@ -20,33 +20,45 @@
 
   ;; windows環境では、cp932-unix,utf-8-unixどちらでも動く
   ;; cp932-unix
-  (setq migemo-coding-system 'cp932-unix)
-  (setq migemo-dictionary "c:/yama/.emacs.d/conf/migemo/dict/cp932/migemo-dict")
+  ;(setq migemo-coding-system 'cp932-unix)
+  ;(setq migemo-dictionary "c:/yama/.emacs.d/conf/migemo/dict/cp932/migemo-dict")
   ;; utf-8-unix
-  ; (setq migemo-coding-system 'utf-8-unix)
-  ;; 辞書ファイルは、Windowsのfile表記形式で設定 migemo.exeがdosベースのコマンドなので。
+  (setq migemo-coding-system 'utf-8-unix) ; no need migemo.el set 'utf-8-unix as default
+  ;; 辞書ファイルは、Windowsのfile表記形式で設定 cmigemo.exeがdosベースのコマンドなので。
   ;; refer http://grugrut.hatenablog.jp/entry/2015/04/emacs-migemo-on-windows "正しい設定"
   ;; migemo-dictionary は辞書ファイルのあるdirectoryを設定する
-  ;(setq migemo-dictionary "c:/yama/.emacs.d/conf/migemo/dict/utf-8/migemo-dict")
+  (setq migemo-dictionary "c:/yama/.emacs.d/conf/migemo/dict/utf-8/migemo-dict")
   )
 
  ((eq system-type 'cygwin)
-  (setq migemo-command "~/bin/cmigemo-default-win64/cmigemo")
-  (setq migemo-options '("-q" "--emacs" "-i" "\a")) ;cygwin ruby では動作せず 調査必要!
+  ;; 'cygwin時も'windows-nt で代用可能かと試したが動作せず
+  (setq migemo-command "~/bin/cmigemo-cygwin/build/cmigemo")
+
+  (setq migemo-options '("-q" "--emacs" "-i" "\a")) ; work
+; (setq migemo-options '("-q" "--emacs" "-i"))      ; work
+; (setq migemo-options '("-q" "--emacs"))           ; work
+
+;  (setq migemo-coding-system 'cp932-unix)
+;  (setq migemo-dictionary "c:/yama/.emacs.d/conf/migemo/dict/cp932/migemo-dict")
+
+  ;; 辞書ファイルは、Windowsのfile表記形式で設定'windows-nt時と同様
   (setq migemo-coding-system 'utf-8-unix)
-  (setq migemo-dictionary "~/.emacs.d/conf/migemo/dict/utf-8/migemo-dict"))
+  (setq migemo-dictionary "c:/yama/.emacs.d/conf/migemo/dict/utf-8/migemo-dict")
+  )
 
  ((eq system-type 'darwin)
   (setq migemo-command "/usr/local/bin/cmigemo")
   (setq migemo-options '("-q" "--emacs"))
   (setq migemo-coding-system 'utf-8-unix)
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"))
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  )
 
  ((eq system-type 'gnu/linux)
   (setq migemo-command "/usr/bin/cmigemo")
   (setq migemo-options '("-q" "--emacs"))
   (setq migemo-coding-system 'utf-8-unix)
-  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict"))
+  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+  )
 )
 
 (load-library "migemo")
