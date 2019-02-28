6c45ecbb25dfa5fda5761cb427a1d867db324788
yamatakau08 <yamatakau08@gmail.com>
2019-02-28 20:27:48 +0900

    modify the number of comments ';' 2 to 3

--- font-setting.el
+++ font-setting.el
@@ -3,12 +3,12 @@
 ;;; you can check the font you are using
 ;;; (w32-select-font)
 
-;; ricty (diminished)
-;; refer the folloing page install
-;; on mac
-;; https://hajipro.com/local-development-environment-mac/ricty-diminished
-;; on windows
-;; http://vdeep.net/ricty-font#WindowsRicty
+;;; ricty (diminished)
+;;; refer the folloing page install
+;;; on mac
+;;; https://hajipro.com/local-development-environment-mac/ricty-diminished
+;;; on windows
+;;; http://vdeep.net/ricty-font#WindowsRicty
 
 ;;; http://www.techscore.com/blog/2016/12/25/emacs-%E3%81%93%E3%81%A8%E3%81%AF%E3%81%98%E3%82%81-%E7%A7%81%E3%81%AE%E4%BA%8B%E4%BE%8B/
 ;;; "フォントを統一する" の項参照
