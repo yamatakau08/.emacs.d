7491ac60c527f8e3eb7843fd6ae2c19af3ee3d49
yamatakau08 <yamatakau08@gmail.com>
2019-03-09 15:51:22 +0900

    revise the most part of code.

--- anki-connect.el
+++ anki-connect.el
@@ -1,7 +1,26 @@
 ;;; 
 (defun my-anki-connect-version ()
+  "get the version"
+  (let ((jsondata (json-encode '((:action  . "version")
+				 (:version . 6)))))
+    (my-anki-connect-push jsondata)))
+
+;;;
+(defun my-anki-connect-sync ()
+  "Anki data sync with Anki Web"
+  (let ((jsondata (json-encode '((:action  . "sync")
+				 (:version . 6)))))
+    (my-anki-connect-push jsondata)))
+
+;;;
+(defun my-anki-connect-push (jsondata)
   ""
-  (let ((cmd (format "curl --noproxy localhost localhost:8765 -X POST -d \"{\\\"action\\\": \\\"version\\\", \\\"version\\\": 6}\"")))
+  (let ((cmd
+	 ;; need --noproxy localhost on Windows Environment
+	 ;; to communicate AnkiConnect and other environmet is available --noproxy option
+	 (format "curl --noproxy localhost localhost:8765 --request POST --data \"%s\""
+		 ;; json data '"' を '\"' に変換
+		 (replace-regexp-in-string "\"" "\\\\\"" jsondata))))
     (message "%s" cmd)
     (shell-command-to-string cmd)))
 
@@ -15,18 +34,23 @@
   ;; defined in ~/.anki-editor
   (my-anki-connect-push-note "英語" from to))
 
+;;;
+(defun my-anki-encode-string (string)
+  "convert string based on AnkiConnect to accept json data with curl command
+   AnkiConnect on Windows accept sjis, other is utf-8"
+  (if (eq system-type 'windows-nt)
+      (encode-coding-string string 'sjis) string))
+
 ;;; json-encode
 ;;; "{\"action\": \"addNote\", \"version\": 6, \"params\": {\"note\": {\"deckName\": \"Default\", \"modelName\": \"Basic\", \"fields\": {\"Front\": \"front content\", \"Back\": \"back content\"}, \"tags\": []}}}"
 ;;; ドット表記のリストで渡すと,"{\"action\":\"addNote\"}" のようになる
 ;;; 入れ子にするには、ドット表記のcdr にリスト (:note (:deckName "Default")) のようにする
 ;;; :action の表記で、json-encode 文字列に変換してくれる
-(defun my-anki-connect-make-json (deck front back)
-  (let ((edeck  (if (eq system-type 'windows-nt)
-		    (encode-coding-string deck  'sjis) deck))
-	(efront (if (eq system-type 'windows-nt)
-		    (encode-coding-string front 'sjis) front))
-	(eback  (if (eq system-type 'windows-nt)
-		    (encode-coding-string back  'sjis) back)))
+(defun my-anki-connect-make-json-addNotes (deck front back)
+  "make json data is availabe AnkiConnect can accept"
+  (let ((edeck  (my-anki-encode-string deck))
+	(efront (my-anki-encode-string front))
+	(eback  (my-anki-encode-string front)))
     (json-encode `((:action  . "addNote")
 		   (:version . 6)
 		   (:params  (:note (:deckName  . ,edeck)
@@ -35,11 +59,14 @@
 						   (:Back  . ,eback)))
 				    (:tags "")))))))
 
+;;;
 (defun my-anki-connect-push-note (deck front back)
+  "Push note which has front and back in deck specified by argments"
   (let ((cmd 
+	 ;; need --noproxy localhost on Windows Environment to communicate AnkiConnect and other environmet is available --noproxy option
 	 (format "curl --noproxy localhost localhost:8765 --request POST --data \"%s\""
 		 ;; json data '"' を '\"' に変換
-		 (replace-regexp-in-string "\"" "\\\\\"" (my-anki-connect-make-json deck front back)))))
+		 (replace-regexp-in-string "\"" "\\\\\"" (my-anki-connect-make-json-addNotes deck front back)))))
     (message "%s" cmd)
     (shell-command-to-string cmd)))
 
@@ -48,7 +75,7 @@
 ;;; option -X is as same as --request
 ;;; refer https://qiita.com/tadsan/items/17d32514b81f1e8f208a
 (defun my-anki-connect-push-notex (deck front back)
-  ""
+  "Push note which has front and back in deck specified by argments"
   (let ((cmd
 	 (format
 	  "curl     \
