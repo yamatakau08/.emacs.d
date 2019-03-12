74b82dbb259391925613c7493a483e304d98c79a
yamatakau08 <yamatakau08@gmail.com>
2019-03-12 16:06:06 +0900

    add condition system-type to call my-anki-editor-push-note

--- .anki-editor.el
+++ .anki-editor.el
@@ -7,9 +7,14 @@
 
 ;;;
 (defun my-anki-editor-push-note (deck front back)
-  "push note into Anki Web"
-  (with-temp-buffer
-    (org-mode)         ; need to push the item to Anki Web via anki-editor
-    (anki-editor-mode) ; need to push the item to Anki Web via anki-editor
-    (insert (format "\n* Item\n  :PROPERTIES:\n  :ANKI_DECK: %s\n  :ANKI_NOTE_TYPE: Basic\n  :END:\n** Front\n   %s\n** Back\n   %s\n" "Default" front back))
-    (anki-editor-push-notes)))
+  "push note into Anki. anki-editor-push-notes works on only Mac, don't work on Windows"
+  (cond
+   ((eq system-type 'darwin)
+    (with-temp-buffer
+      (org-mode)         ; need to push the item to Anki Web via anki-editor
+      (anki-editor-mode) ; need to push the item to Anki Web via anki-editor
+      (insert (format "\n* Item\n  :PROPERTIES:\n  :ANKI_DECK: %s\n  :ANKI_NOTE_TYPE: Basic\n  :END:\n** Front\n   %s\n** Back\n   %s\n" "Default" front back))
+      (anki-editor-push-notes)))
+   (t (message "%s: anki-editor-push-notes doesnt support!" system-type)
+      nil) ; if there is not nil, error "command-execute: Wrong type argument: listp," occurs on call side
+   ))
