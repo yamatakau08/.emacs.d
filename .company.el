9b8df87d7fc1fadf971e92066e2d7c8095dcaae5
yamatakau08 <yamatakau08@gmail.com>
2019-03-27 09:51:02 +0900

    modify to use "use-package"'s ensure and config function.

--- .company.el
+++ .company.el
@@ -1,9 +1,23 @@
-;;; https://company-mode.github.io/
-;;; enable company mode in all buffer
-;(global-company-mode t)
-(add-hook 'after-init-hook 'global-company-mode)
+;;; company official site: https://company-mode.github.io/
 
-;;; refer https://qiita.com/kod314/items/3a31719db27a166d2ec1#%E8%A8%AD%E5%AE%9A%E3%81%97%E3%81%9F%E3%81%93%E3%81%A8
-;;; C-n, C-pで補完候補を次/前の候補を選択
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
+;;; to suppress error when emacs launch
+;;; error: eval-buffer: Symbol’s value as variable is void: company-active-map
+;;; comment the following to use use-package
+;(require 'company)
+
+;;; to https://kumaroot.readthedocs.io/ja/latest/emacs-use-package.html#id2
+(use-package company
+  :ensure t
+  :config
+  ;; enable company mode in all buffer
+  ;; (global-company-mode t)
+  (add-hook 'after-init-hook 'global-company-mode)
+
+  ;; to use C-n C-p to select next/prev candidate
+  ;; refer https://qiita.com/kod314/items/3a31719db27a166d2ec1#%E8%A8%AD%E5%AE%9A%E3%81%97%E3%81%9F%E3%81%93%E3%81%A8
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+
+
+
