8dabde3dbba659677e29a3d08bcb9b1e11d4c2fc
yamatakau08 <yamatakau08@gmail.com>
2019-04-08 19:58:55 +0900

    add my-wl-source-get function and modify load ssl.el with that function.

--- .wl.el
+++ .wl.el
@@ -6,11 +6,44 @@
 (setq elmo-imap4-debug t)
 (setq elmo-pop3-debug  t)
 
+;;; should be defun before being called.
+;;; https://qiita.com/tadsan/items/17d32514b81f1e8f208a
+(defun my-wl-source-get ()
+  "get wanderlust source from wanderlust github repository"
+  (interactive)
+  (let* ((repository-name "wanderlust")
+	 (default-directory (expand-file-name "~/.emacs.d"))
+	 (repository "https://github.com/wanderlust/wanderlust.git")
+	;; https://qiita.com/tadsan/items/17d32514b81f1e8f208a#%E3%81%AE%E5%B1%95%E9%96%8B
+	 (destdir    (expand-file-name (concat default-directory "/" repository-name))))
+    (if (file-directory-p destdir)
+	;; https://qiita.com/tadsan/items/17d32514b81f1e8f208a#default-directory%E3%81%AB%E6%B0%97%E3%82%92%E9%85%8D%E3%82%8B
+	(progn
+	  ;; (message "default-dirrectory:%s, directory:%s exists" default-directory destdir)
+	  (message "pulling %s into %s" repository-name destdir)
+	  (shell-command-to-string
+	   (mapconcat #'shell-quote-argument
+		      (list "git" "pull")
+		      " "))
+	  )
+      ;; else
+      (message "cloning %s into %s" repository-name destdir)
+      (shell-command-to-string
+       (mapconcat #'shell-quote-argument
+		  (list "git" "clone" repository destdir)
+		  " "))
+      (message "done cloning %s into %s" repository-name destdir)
+      )))
+
 ;;; when launch wl puts out errro
 ;;; "wl: Cannot open load file: No such file or directory, ssl"
 ;;; need to use openssl in https://github.com/wanderlust/wanderlust/blob/master/utils/ssl.el
 ;;; Since package doesn't install ssl.el, install manualy.
-(my-load "~/.emacs.d/wanderlust/utils/ssl.el")
+(let ((file (expand-file-name "~/.emacs.d/wanderlust/utils/ssl.el")))
+  (if (file-exists-p file)
+      (load file)
+    (my-wl-source-get)
+    (load file)))
 
 ;;; for imap connection
 ;;; ssl-program-name default value is "openssl"
