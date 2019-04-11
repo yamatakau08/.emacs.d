21b64db5de376de7f7a28d3faf47dbfa4c0e1c0d
yamatakau08 <yamatakau08@gmail.com>
2019-04-11 10:12:49 +0900

    change to use "require" to load my-anki-connect.el and my-anki-connect-push-note*x*

--- anki.el
+++ anki.el
@@ -3,8 +3,8 @@
 ;;;
 
 ;;; for adding note in Anki
-(add-to-list 'load-path "~/.emacs.d/my-anki-connect")
-(require 'my-anki-connect)
+;(add-to-list 'load-path "~/.emacs.d/my-anki-connect")
+(require 'my-anki-connect "~/.emacs.d/my-anki-connect/my-anki-connect.el")
 
 ;;;
 (defun my-anki-add-note-english ()
@@ -13,6 +13,6 @@
   (let ((deck "英語")
 	(front (read-string "Front: "))
 	(back  (read-string "Back : ")))
-    (my-anki-connect-push-note deck front back)))
+    (my-anki-connect-push-notex deck front back)))
 
 (global-set-key "\C-cr" 'my-anki-add-note-english)
