a733999d4e30e2172529ceaebd12cdbfe4dd2bcc
yamatakau08 <yamatakau08@gmail.com>
2019-04-19 14:42:12 +0900

    add request-curl-options setting and the process of anki-editor-push-notes and ammend some comments

--- .anki-editor.el
+++ .anki-editor.el
@@ -1,9 +1,16 @@
-;;;
+;;; for anki-editor https://github.com/louietan/anki-editor
+
+;;; launch Anki app
 ;(cond
 ; ((eq system-type 'darwin)
 ;  (start-process "Anki" nil "/Applications/Anki.app/Contents/MacOS/Anki")))
 
-(require 'anki-editor) ; to register item to upload the note directly in Anki Web
+;;; to register item to upload the note directly in Anki Web
+(require 'anki-editor)
+
+;;;
+(with-eval-after-load "request"
+  (setq request-curl-options "-noproxy")) ; for windows environment but doesn't work
 
 ;;;
 (defun my-anki-editor-push-note (deck front back)
@@ -15,7 +22,13 @@
       (anki-editor-mode) ; need to push the item to Anki Web via anki-editor
       (insert (format "\n* Item\n  :PROPERTIES:\n  :ANKI_DECK: %s\n  :ANKI_NOTE_TYPE: Basic\n  :END:\n** Front\n   %s\n** Back\n   %s\n" "Default" front back))
       (anki-editor-push-notes)))
+   ((eq system-type 'windows-nt)
+    (with-temp-buffer
+      (org-mode)         ; need to push the item to Anki Web via anki-editor
+      (anki-editor-mode) ; need to push the item to Anki Web via anki-editor
+      (insert (format "\n* Item\n  :PROPERTIES:\n  :ANKI_DECK: %s\n  :ANKI_NOTE_TYPE: Basic\n  :END:\n** Front\n   %s\n** Back\n   %s\n" "Default" front back))
+      (anki-editor-push-notes)))
    (t (message "%s: anki-editor-push-notes doesnt support!" system-type))
    )
-  nil ; if there is not nil, error "command-execute: Wrong type argument: listp," occurs on call side
+  nil ; if there is not nil, error "command-execute: Wrong type argument: listp," occurs on caller side
 )
