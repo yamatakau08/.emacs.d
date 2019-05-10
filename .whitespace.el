4a198f8080d988af05d25d0dad70ddcac98307c4
yamatakau08 <yamatakau08@gmail.com>
2019-05-10 15:21:20 +0900

    change settings and use dolist.

--- .whitespace.el
+++ .whitespace.el
@@ -1,6 +1,13 @@
 (require 'whitespace)
-(delq 'lines whitespace-style) ; delete lines check over 80 column set by whitespace-line-column
-;; need to study not to display zenkaku space on whitespace-mode on Windows Myrica font
+
+;;(delq 'lines whitespace-style)      ; delete lines check over 80 column set by whitespace-line-column
+;;(delq 'spaces whitespace-style)     ; not to highlite SPACEs and HARD SPACEs are visualized via faces.
+;;(delq 'space-mark whitespace-style) ; not to display "table.SPACEs" and HARD SPACEs are visualized via faces.
+;;(delq 'newline-mark whitespace-style) ; NEWLINEs are visualized via display table.
+
+(dolist (val '(lines spaces space-mark newline-mark))
+  (delq val whitespace-style))
+
 
 
 
