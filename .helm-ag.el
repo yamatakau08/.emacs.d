d7d8b61956f3287651741221f465f20d54c33d7c
yamatakau08 <yamatakau08@gmail.com>
2019-05-22 16:26:22 +0900

    add /usr/local/bin path for helm-ag ag command.

--- .helm-ag.el
+++ .helm-ag.el
@@ -1,2 +1,9 @@
 (use-package helm-ag
   :ensure t)
+
+;;; to install "ag"
+;;; $ brew install ag, ag is installed in /usr/local/bin on mac
+;;; unfortunately, exec-path doesn't include /usr/local/bin even if that path is set in .bash_profile.
+;;; tentative add /usr/local/bin in exec-path
+(if (eq system-type 'darwin)
+    (add-to-list 'exec-path "/usr/local/bin"))
