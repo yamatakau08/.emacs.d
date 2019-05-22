91f7d9b00dcc8b53a57242a8c540c1ca09a7f3e8
yamatakau08 <yamatakau08@gmail.com>
2019-05-22 15:19:28 +0900

    delete blank lines at the end of file.

--- .company.el
+++ .company.el
@@ -17,7 +17,3 @@
   ;; refer https://qiita.com/kod314/items/3a31719db27a166d2ec1#%E8%A8%AD%E5%AE%9A%E3%81%97%E3%81%9F%E3%81%93%E3%81%A8
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
-
-
-
-
