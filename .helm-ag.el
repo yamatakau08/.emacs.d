98a523245cfe4a26eff9cc2d41ec6f4cdeefdcda
yamatakau08 <yamatakau08@gmail.com>
2019-05-22 17:50:35 +0900

    add (setq helm-follow-mode-persistent t) to follow the action in helm-ag candidate buffer.

--- .helm-ag.el
+++ .helm-ag.el
@@ -1,2 +1,7 @@
 (use-package helm-ag
   :ensure t)
+
+;;; https://github.com/syohex/emacs-helm-ag#enable-helm-follow-mode-by-default
+;;; in helm-ag candidate buffer, selected search result with ctr-n or ctr-p
+;;; other window follow that action and shows the part you selected in candidate buffer
+(setq helm-follow-mode-persistent t)
