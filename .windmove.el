7144f75dfa148fb803116f4157ac71f61c076940
yamatakau08 <yamatakau08@gmail.com>
2019-05-23 10:28:55 +0900

    align 'windomove-* and that right parenthesis

--- .windmove.el
+++ .windmove.el
@@ -11,9 +11,9 @@
 ;;; refer https://orgmode.org/manual/Conflicts.html
 ;;; windmove
 ;; Make windmove work in Org mode:
-(add-hook 'org-shiftup-final-hook 'windmove-up)
-(add-hook 'org-shiftleft-final-hook 'windmove-left)
-(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftup-final-hook    'windmove-up   )
+(add-hook 'org-shiftleft-final-hook  'windmove-left )
+(add-hook 'org-shiftdown-final-hook  'windmove-down )
 (add-hook 'org-shiftright-final-hook 'windmove-right)
 
 (setq windmove-wrap-around t)
