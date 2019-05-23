53243f543ab294713b65972c83f7c63c95cd73e9
yamatakau08 <yamatakau08@gmail.com>
2019-05-23 12:05:53 +0900

    move (setq helm-follow-mode-persistent t) in .helm.el from .helm-ag.el

--- .helm-ag.el
+++ .helm-ag.el
@@ -10,5 +10,5 @@
 
 ;;; https://github.com/syohex/emacs-helm-ag#enable-helm-follow-mode-by-default
 ;;; in helm-ag candidate buffer, selected search result with ctr-n or ctr-p
-;;; other window follow that action and shows the part you selected in candidate buffer
-(setq helm-follow-mode-persistent t)
+;;; other window follow that action and shows the part selected in candidate buffer
+;;(setq helm-follow-mode-persistent t) ; since this setting is for helm itself, move to .helm.el
--- .helm.el
+++ .helm.el
@@ -5,8 +5,15 @@
 ;(require 'helm-config) ; https://github.com/emacs-helm/helm/wiki#install
 (require 'helm) ; helm-config を require すると、helm-migiemo-mode を設定するとエラーになるので、(require 'helm) する
 
+;;
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x f") 'helm-find-files)
 ;(global-set-key (kbd "C-x o") 'helm-buffers-list) ; to tentative setting
 
 (helm-migemo-mode 1)
+
+;;; https://github.com/syohex/emacs-helm-ag#enable-helm-follow-mode-by-default
+;;; in helm-ag candidate buffer, selected search result with ctr-n or ctr-p
+;;; other window follow that action and shows the part selected in candidate buffer
+;;; need to execcute C-c C-f in helm minibuffer to enable helm-follow-mode
+(setq helm-follow-mode-persistent t)
