55be7e2d0739fbc83a3344724cc6782db7eb51b5
yamatakau08 <yamatakau08@gmail.com>
2019-10-18 14:13:56 +0900

    combine the block in case of darwin and windows-nt

--- .anki-editor.el
+++ .anki-editor.el
@@ -16,13 +16,8 @@
 (defun my-anki-editor-push-note (deck front back)
   "push note into Anki. anki-editor-push-notes works on only Mac, don't work on Windows"
   (cond
-   ((eq system-type 'darwin)
-    (with-temp-buffer
-      (org-mode)         ; need to push the item to Anki Web via anki-editor
-      (anki-editor-mode) ; need to push the item to Anki Web via anki-editor
-      (insert (format "\n* Item\n  :PROPERTIES:\n  :ANKI_DECK: %s\n  :ANKI_NOTE_TYPE: Basic\n  :END:\n** Front\n   %s\n** Back\n   %s\n" "Default" front back))
-      (anki-editor-push-notes)))
-   ((eq system-type 'windows-nt)
+   ((or (eq system-type 'darwin)
+	(eq system-type 'windows-nt))
     (with-temp-buffer
       (org-mode)         ; need to push the item to Anki Web via anki-editor
       (anki-editor-mode) ; need to push the item to Anki Web via anki-editor
