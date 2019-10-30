1a0089e264fe6ddfbc428c18ead60f727da5cd48
yamatakau08 <yamatakau08@gmail.com>
2019-10-30 18:05:06 +0900

    .

--- .helm-ag.el
+++ .helm-ag.el
@@ -51,50 +51,50 @@
 ;;; replace function of "find-file" for helm-ag--find-file-action
 ;;; when calling helm-ag-this-file and result buffer with search string
 ;;; during target-file is always updating
-(defun helm-ag--find-file-action-find-file (filename &optional wildcards)
-  "Edit file FILENAME.
-Switch to a buffer visiting file FILENAME,
-creating one if none already exists.
-Interactively, the default if you just type RET is the current directory,
-but the visited file name is available through the minibuffer history:
-type \\[next-history-element] to pull it into the minibuffer.
-
-The first time \\[next-history-element] is used after Emacs prompts for
-the file name, the result is affected by `file-name-at-point-functions',
-which by default try to guess the file name by looking at point in the
-current buffer.  Customize the value of `file-name-at-point-functions'
-or set it to nil, if you want only the visited file name and the
-current directory to be available on first \\[next-history-element]
-request.
-
-You can visit files on remote machines by specifying something
-like /ssh:SOME_REMOTE_MACHINE:FILE for the file name.  You can
-also visit local files as a different user by specifying
-/sudo::FILE for the file name.
-See the Info node `(tramp)File name Syntax' in the Tramp Info
-manual, for more about this.
-
-Interactively, or if WILDCARDS is non-nil in a call from Lisp,
-expand wildcards (if any) and visit multiple files.  You can
-suppress wildcard expansion by setting `find-file-wildcards' to nil.
-
-To visit a file without any kind of conversion and without
-automatically choosing a major mode, use \\[find-file-literally]."
-  (interactive
-   (find-file-read-args "Find file: "
-                        (confirm-nonexistent-file-or-buffer)))
-  (let ((value (find-file-noselect filename t nil wildcards))) ; force set to 2'nd arg t of find-file-noselect
-    (if (listp value)
-	(mapcar 'pop-to-buffer-same-window (nreverse value))
-      (pop-to-buffer-same-window value))))
+;(defun helm-ag--find-file-action-find-file (filename &optional wildcards)
+;  "Edit file FILENAME.
+;Switch to a buffer visiting file FILENAME,
+;creating one if none already exists.
+;Interactively, the default if you just type RET is the current directory,
+;but the visited file name is available through the minibuffer history:
+;type \\[next-history-element] to pull it into the minibuffer.
+;
+;The first time \\[next-history-element] is used after Emacs prompts for
+;the file name, the result is affected by `file-name-at-point-functions',
+;which by default try to guess the file name by looking at point in the
+;current buffer.  Customize the value of `file-name-at-point-functions'
+;or set it to nil, if you want only the visited file name and the
+;current directory to be available on first \\[next-history-element]
+;request.
+;
+;You can visit files on remote machines by specifying something
+;like /ssh:SOME_REMOTE_MACHINE:FILE for the file name.  You can
+;also visit local files as a different user by specifying
+;/sudo::FILE for the file name.
+;See the Info node `(tramp)File name Syntax' in the Tramp Info
+;manual, for more about this.
+;
+;Interactively, or if WILDCARDS is non-nil in a call from Lisp,
+;expand wildcards (if any) and visit multiple files.  You can
+;suppress wildcard expansion by setting `find-file-wildcards' to nil.
+;
+;To visit a file without any kind of conversion and without
+;automatically choosing a major mode, use \\[find-file-literally]."
+;  (interactive
+;   (find-file-read-args "Find file: "
+;                        (confirm-nonexistent-file-or-buffer)))
+;  (let ((value (find-file-noselect filename t nil wildcards))) ; force set to 2'nd arg t of find-file-noselect
+;    (if (listp value)
+;	(mapcar 'pop-to-buffer-same-window (nreverse value))
+;      (pop-to-buffer-same-window value))))
 
 (defun helm-ag--find-file-action--filter-args (args)
-  ;(print args) ; for debug
+  ;;(print args) ; for debug
   (if (eq (nth 1 args) 'find-file)
       (setf (nth 1 args) (lambda (filename) (switch-to-buffer (find-file-noselect filename t))))
       ;(setf (nth 1 args) 'helm-ag--find-file-action-find-file)
     )
-  ;(print args) ; for debug
+  ;;(print args) ; for debug
   args)
 
 (advice-add 'helm-ag--find-file-action :filter-args
