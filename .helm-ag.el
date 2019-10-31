a8f8365741c559fd5a82bfa87997c42baa9f79e5
yamatakau08 <yamatakau08@gmail.com>
2019-10-31 21:18:06 +0900

    .

--- .helm-ag.el
+++ .helm-ag.el
@@ -99,3 +99,55 @@
 
 (advice-add 'helm-ag--find-file-action :filter-args
 	    #'helm-ag--find-file-action--filter-args)
+
+;;;
+;(custom-set-variables '(helm-ag-command-option "--depth 0"))
+(defun my-helm-do-ag (&optional basedir targets)
+  (interactive)
+  (require 'helm-mode)
+  (helm-ag--init-state)
+  (let* ((helm-ag-command-option "--depth 0") ; for test
+	 (helm-ag--default-directory (or basedir default-directory))
+         (helm-ag--default-target (cond (targets targets)
+                                        ((and (helm-ag--windows-p) basedir) (list basedir))
+                                        (t
+                                         (when (and (not basedir) (not helm-ag--buffer-search))
+                                           (helm-read-file-name
+                                            "Search in file(s): "
+                                            :default default-directory
+                                            :marked-candidates t :must-match t)))))
+         (helm-do-ag--extensions (when helm-ag--default-target
+                                   (helm-ag--do-ag-searched-extensions)))
+         (one-directory-p (helm-do-ag--target-one-directory-p
+                           helm-ag--default-target)))
+    (helm-ag--set-do-ag-option)
+    (helm-ag--set-command-features)
+    (helm-ag--save-current-context)
+    (helm-attrset 'search-this-file
+                  (and (= (length helm-ag--default-target) 1)
+                       (not (file-directory-p (car helm-ag--default-target)))
+                       (car helm-ag--default-target))
+                  helm-source-do-ag)
+    (if (or (helm-ag--windows-p) (not one-directory-p)) ;; Path argument must be specified on Windows
+        (helm-do-ag--helm)
+      (let* ((helm-ag--default-directory
+              (file-name-as-directory (car helm-ag--default-target)))
+             (helm-ag--default-target nil))
+        (helm-do-ag--helm)))))
+
+;;; > ag --list-file-types
+;;; The following file types I offten use are supported:
+;;;  --batch
+;;;     .bat  .cmd
+;;;  --elisp
+;;;      .el
+;;;  --log
+;;;      .log
+;;;  --org
+;;;      .org
+;;;  --python
+;;;      .py
+;;;  --ruby
+;;;      .rb  .rhtml  .rjs  .rxml  .erb  .rake  .spec
+;;;  --shell
+;;;      .sh  .bash  .csh  .tcsh  .ksh  .zsh  .fish
