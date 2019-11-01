d7d935fe52f49622dc65346bda55fed8b557bf65
yamatakau08 <yamatakau08@gmail.com>
2019-11-01 19:27:52 +0900

    amend the typo in comment .

--- .ansi-color.el
+++ .ansi-color.el
@@ -1,5 +1,4 @@
 (defun my-ansi-color-apply ()
-  "to clor ansi escape sequence in buffer
-supported by emacs-jp slack"
+  "to color ansi escape sequence in buffer supported by emacs-jp slack"
   (interactive)
   (ansi-color-apply-on-region (point-min) (point-max)))
