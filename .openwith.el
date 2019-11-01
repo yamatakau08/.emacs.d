225ea85db1cf0b4216f9987b56c7e1dc8587d934
yamatakau08 <yamatakau08@gmail.com>
2019-11-01 19:33:57 +0900

    add file suffixes.

--- .openwith.el
+++ .openwith.el
@@ -9,13 +9,21 @@
    ((eq system-type 'windows-nt)
     (setq openwith-associations
 	  (list
-	   '("\\.pptx" nil (file)) ; nil: Since "open" in specified in openwith.el selects application associated with file suffix, we don't need to specify application program.
+	   ;; '("\\.png"  "open" (file)) ; open is also ok.
+	   ;; '("\\.pptx" nil    (file)) ; nil: Since "open" in specified in openwith.el selects application associated with file suffix, we don't need to specify application program.
+	   ;; office
 	   (list (openwith-make-extension-regexp
-		  '("xlsx" "xlsm"))
+		  '("pptx" "xlsx" "xlsm" "docx"))
+		 nil
+		 '(file))
+	   ;; other than office
+	   (list (openwith-make-extension-regexp
+		  '("pdf" "msg" "jpg" "png" "mp3" "mp4"))
 		 nil
 		 '(file))))
     )
    ((eq system-type 'darwin)
+    ;; need to study alert sound is heared when open file
     (setq openwith-associations
 	  (list
 	   '("\\.pdf"     "open" (file))
@@ -28,7 +36,7 @@
 (defun openwith-open-windows-filter-args (arg)
   ;; this filter advice function
   ;; arg is list ("//jps00004944/share_data/SQA1-HA/10_Test_Project_FY17/NewAudio/02_検証計画書/Schedule-NewAudio-Evaluation.pptx")
-  ;; replace file path in car of arg with my-app-open-file-path2explore
+  ;; replace file path converted with my-app-open-file-path2explore in car of arg list
   ;;; "\\\\jps00004944\\share_data\\SQA1-HA\\10_Test_Project_FY17\\NewAudio\\02_検証計画書\\Schedule-NewAudio-Evaluation.pptx"
   (list (my-app-open-file-path2explore (car arg)))
   )
