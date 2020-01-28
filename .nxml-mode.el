4d6b0beabc832596d722ff8fa92f04cf78519454
yamatakau08 <yamatakau08@gmail.com>
2020-01-28 09:27:33 +0900

    comment not to call my-sgml-pretty-print.

--- .nxml-mode.el
+++ .nxml-mode.el
@@ -1,6 +1,7 @@
 ;; refer https://emacs-jp.slack.com/messages/C6T2T9H4G/
 (defun my-nxml-mode-hook ()
   (hs-minor-mode 1)
-  (my-sgml-pretty-print))
+  ;;(my-sgml-pretty-print) ; when open xml files, this function call sgml-pretty-print and add new line "</...>"
+  )
 
 (add-hook 'nxml-mode-hook 'my-nxml-mode-hook)
