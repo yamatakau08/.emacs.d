025e17c9e4fb1a2c49d6c78514fe399cfb3a9d0b
yamatakau08 <yamatakau08@gmail.com>
2020-04-27 13:00:16 +0900

    .

--- .eced.el
+++ .eced.el
@@ -1,3 +1,3 @@
 ;(add-to-list 'load-path "~/bin/sqlite3_sample")
-(require 'eced      "~/bin/sqlite3_sample/eced.el")
-(require 'eced-menu "~/bin/sqlite3_sample/eced-menu.el")
+(require 'eced      "~/bin/sqlite3_sample/eced.el"      t) ; t: NOERROR
+(require 'eced-menu "~/bin/sqlite3_sample/eced-menu.el" t) ; t: NOERROR
