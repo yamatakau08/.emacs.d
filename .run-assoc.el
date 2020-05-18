dff7a55cdb048021b60683064cd620d9a6d563f8
yamatakau08 <yamatakau08@gmail.com>
2020-05-18 22:16:11 +0900

    amend comment.

--- .run-assoc.el
+++ .run-assoc.el
@@ -21,7 +21,7 @@
  ((eq system-type 'windows-nt)
   (setq associated-program-alist
 	'(((lambda (file)
-	     ;; note: sinc if html is in file suffix regexp, helm-find-file can't open url which has html,
+	     ;; note: if html is in file suffix regexp, helm-find-file can't open url which has html,
 	     ;; I don't include htlm in file suffix regexp
 	     ;;(w32-shell-execute "open" file)) "\\.\\(pdf\\|png\\|JPG\\|msg\\|pptx\\|xls\\|xlsx\\|xlsm\\|docx\\|avi\\|mp4\\)$")))
 	     ;; don't include JPG/png image file suffixes, they are appeared in the emacs buffer with normal file open
