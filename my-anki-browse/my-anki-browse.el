b3e2d116971a74e1326091b98c3d13918941e0fe
yamatakau08 <yamatakau08@gmail.com>
2020-06-21 00:50:42 +0900

    .

--- my-anki-browse/my-anki-browse.el
+++ my-anki-browse/my-anki-browse.el
@@ -21,108 +21,90 @@
 ;;; Commentary:
 
 ;; Todo
-;; my-anki-browse-anki-version
-;; [my-anki-browse] -deckNamesAndIds: ((result (英語 . 1550242731310) (国語 . 1550372251942) (カスタム学習セッション . 1550243694498) (その他 . 1554811643083) (Default . 1)) (error))
+;;
+
+;; meme
+;; (my-anki-browse-deckNamesAndIds)
+;; => ((result (英語 . 1550242731310) (国語 . 1550372251942) (カスタム学習セッション . 1550243694498) (その他 . 1554811643083) (Default . 1)) (error))
+;;
+;; arg: deck name
 ;;(my-anki-browse-findNotes "その他")
-;;[1554811649049 1565823907156]
+;; => [1554811649049 1565823907156]
 ;;
-;;(defun my-anki-browse-cardsInfo (cardids)
-;;  (json-encode
-;;   `((:action  . "cardsInfo")
-;;     (:version . ,my-anki-browse-anki-version)
-;;     (:params  (:cards ,cardids)))))
-;;
-;;(my-anki-browse-cardsInfo (my-anki-browse-findNotes "その他"))
-;;"{\"action\":\"cardsInfo\",\"version\":6,\"params\":{\"cards\":[[1554811649049,1565823907156]]}}"
+;; 何故かよく分からないが、notesInfo に、noteid を渡すと、その noteid の情報が得られる
+;; card だと思うのだが...
+;; (my-anki-browse-notesInfo 1554811649049)
+;; => ((result . [((noteId . 1554811649049) (tags . []) (fields (Front (value . ディアスポラ) (order . 0)) (Back (value . ギリシャ語 離散の意味<br>パレスチナ以外に移り住んだユダヤ人とその社会を指す<br>今は、故郷や祖先の地を離れ暮らす人やコミュニティを指す) (order . 1))) (modelName . Basic) (cards . [1554811848515]))]) (error))
 
 ;;; Code:
 (require 'request)
 
 ;; for debug
-;(setq request-log-level     'debug)
-;(setq request-message-level 'debug)
+(setq request-log-level     'debug)
+(setq request-message-level 'debug)
 
 ;;
 (defvar my-anki-browse-anki-connect-url "localhost:8765")
-(defvar my-anki-browse-anki-version 6)
+(defvar my-anki-browse-anki-connect-version nil)
+(defvar my-anki-browse-anki-deck-names nil)
 
-(defun my-anki-browse-cardsInfo (cardids)
-  "Returns a list of objects containing for each card ID the card fields,
-front and back sides including CSS, note type,
-the note that the card belongs to, and deck name, as well as ease and interval.
-https://github.com/FooSoft/anki-connect/blob/master/actions/cards.md"
-  (interactive "ncardIDs: ")
-  (request
-    my-anki-browse-anki-connect-url
-    :sync t
-    :type "POST"
-    :headers '(("Content-Type" . "application/json"))
-    :data (json-encode
-	   `((:action  . "cardsInfo")
-	     (:version . ,my-anki-browse-anki-version)
-	     (:params  (:cards ,cardids))))
-    :parser 'json-read ; parse-error occurs without json-encode at ":data" part
-    :success (cl-function
-	      (lambda (&key data &allow-other-keys)
-		(message "[my-anki-browse] -cardsInfo: %s" data)))))
+;;
+(defun my-anki-browse-deck-cards (deck)
+  ;; https://stackoverflow.com/questions/2382524/adding-completion-to-interactive/2382677#2382677
+  ;; to list up the completion with interactive
+  (interactive
+   (list
+    (completing-read "Deck: " (my-anki-browse-deckNames))))
+  (let (noteids)
+    (setq noteids (my-anki-browse-findNotes deck))
+    (my-anki-browse-notesInfo noteids)))
 
 (defun my-anki-browse-notesInfo (noteids)
   "Returns a list of objects containing for each note ID the note fields, tags, note type and the cards belonging to the note.
-https://github.com/FooSoft/anki-connect/blob/master/actions/notes.md"
+https://github.com/FooSoft/anki-connect/blob/master/actions/notes.md
+noteids: array [1,2]"
   (interactive "nnoteIDs: ")
-  (request
-    my-anki-browse-anki-connect-url
-    :sync t
-    :type "POST"
-    :headers '(("Content-Type" . "application/json"))
-    :data (json-encode
-	   `((:action  . "notesInfo")
-	     (:version . ,my-anki-browse-anki-version)
-	     (:params  (:notes ,noteids))))
-    :parser 'json-read ; parse-error occurs without json-encode at ":data" part
-    :success (cl-function
-	      (lambda (&key data &allow-other-keys)
-		(message "[my-anki-browse] -notesInfo: %s" data)))))
+  (my-anki-browse-request
+   :data (json-encode
+	  `((:action  . "notesInfo")
+	    (:version . ,my-anki-browse-anki-connect-version)
+	    (:params  (:notes . ,noteids))))))
 
 (defun my-anki-browse-findNotes (deck)
   "Returns an array of note IDs for a given query. Same query syntax as guiBrowse.
-[my-anki-browse] -findNotes: ((result . [1559778375256 1580531127255 1581461417451 1582239433730]) (error))
 https://github.com/FooSoft/anki-connect/blob/master/actions/notes.md"
   (interactive "sDeck: ")
-  (let (cardids)
-    (request
-      my-anki-browse-anki-connect-url
-      :sync t
-      :type "POST"
-      :headers '(("Content-Type" . "application/json"))
-      :data (json-encode
-	     `((:action  . "findNotes")
-	       (:version . ,my-anki-browse-anki-version)
-	       ;;(:params  (:query . "deck:current")))) ; sample
-	       (:params  (:query . ,(format "deck:%s" deck))))) ; "," before "(format" is necessary!
-      :parser 'json-read ; parse-error occurs without json-encode at ":data" part
-      :success (cl-function
-		(lambda (&key data &allow-other-keys)
-		  (message "[my-anki-browse] -findNotes: %s" data) ; for debug
-		  (setq cardids (let-alist data .result)))))
-    cardids))
+  (my-anki-browse-request
+   :data (json-encode
+	  `((:action  . "findNotes")
+	    (:version . ,my-anki-browse-anki-connect-version)
+	    ;;(:params  (:query . "deck:current")))) ; sample
+	    (:params  (:query . ,(format "deck:%s" deck))))))) ; "," before "(format" is necessary!
+
+(defun my-anki-browse-deckNames ()
+  "Gets the complete list of deck names for the current user.
+https://github.com/FooSoft/anki-connect/blob/master/actions/decks.md#deck-actions"
+  (interactive)
+  (let (deck-names)
+    (setq deck-names
+	  (my-anki-browse-request
+	   :type "POST"
+	   :data (json-encode
+		  `(("action"  . "deckNames")
+		    ("version" . ,my-anki-browse-anki-connect-version)))))
+    ;; coerce http://dminor11th.blogspot.com/2012/06/coerce.html?m=1
+    ;; to convert array to list to utilize the deck names
+    (setq my-anki-browse-anki-deck-names (coerce deck-names 'list))))
 
 (defun my-anki-browse-deckNamesAndIds ()
   "Gets the complete list of deck names and their respective IDs for the current user.
 https://github.com/FooSoft/anki-connect/blob/master/actions/decks.md#deck-actions"
   (interactive)
-  (request
-    my-anki-browse-anki-connect-url
-    :sync t
-    :type "POST"
-    :headers '(("Content-Type" . "application/json"))
-    :data (json-encode
-	   `(("action"  . "deckNamesAndIds")
-	     ("version" . ,my-anki-browse-anki-version)))
-    :parser 'json-read ; parse-error occurs without json-encode at ":data" part
-    :success (cl-function
-	      (lambda (&key data &allow-other-keys)
-		(message "[my-anki-browse] -deckNamesAndIds: %s" data)))))
+  (my-anki-browse-request
+   :type "POST"
+   :data (json-encode
+	  `(("action"  . "deckNamesAndIds")
+   	    ("version" . ,my-anki-browse-anki-connect-version)))))
 
 (defun my-anki-browse-version ()
   "Gets the version of the API exposed by this plugin. Currently versions 1 through 6 are defined.
@@ -135,11 +117,47 @@ https://github.com/FooSoft/anki-connect/blob/master/actions/miscellaneous.md"
     :headers '(("Content-Type" . "application/json"))
     :data (json-encode
 	   `((:action  . "version")))
-	     ;;(:version . 6))) ;; without version, response return version 6
+           ;;(:version . 6))) ;; without version, anki-connect returns version 6
     :parser 'json-read ; parse-error occurs without json-encode at ":data" part
     :success (cl-function
 	      (lambda (&key data &allow-other-keys)
-		(message "[my-anki-browse] -version: %s" data)))))
+		(setq my-anki-browse-anki-connect-version data)
+		(message "[my-anki-browse] -version: %s" data)))
+    :error (cl-function
+	    (lambda (&rest args &key error-thrown &allow-other-keys)
+	      (message "[my-anki-browse] -version error: %s" error-thrown)))))
+
+;; private
+(defun my-anki-browse-request (&rest args)
+  (unless my-anki-browse-anki-connect-version
+    (my-anki-browse-version))
+
+  (message "[my-anki-browse][debug] -request args: %s" args)
+
+  (if my-anki-browse-anki-connect-version
+      (let (response
+	    data
+	    result
+	    error)
+	(setq response
+	      (apply #'request
+		     my-anki-browse-anki-connect-url
+		     :sync t
+		     :headers '(("Content-Type" . "application/json"))
+		     :parser  'json-read ;'json-read ; note that numerice has decimal point
+		     :success (cl-function
+			       (lambda (&key data &allow-other-keys)
+				 (message "[my-anki-browse][debug] -request: %s" data)))
+		     args)) ; args are ":type",":data" ... , and should be the last
+	(setq data (request-response-data response))
+	(setq result (let-alist data .result))
+	(setq error  (let-alist data .error))
+	(if error
+	    (message "[my-anki-browse] -request error: %s" error))
+	(message "[my-anki-browse] -request result: %s" result)
+	result) ; return the data
+    (message "[my-anki-browse] get-version error!")
+    (message "[my-anki-browse] anki doesn't seem to be launched!")))
 
 (provide 'my-anki-browse)
 ;;; my-anki-browse.el ends here
