94eb68e9db4dfb3ea97de61c9e8d0354943d5d65
yamatakau08 <yamatakau08@gmail.com>
2020-08-19 19:13:31 +0900

    .

--- init.el
+++ init.el
@@ -228,6 +228,8 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 (my-load "~/.emacs.d/.frog-jump-buffer.el")
 
+(my-load "~/.emacs.d/.tabbar.el")
+
 ;; custom-set-variables,faces is automatically added in the end of init.el
 ;; when that function uses variables are executed.
 ;; To avoid to that, use the following settings
