2cf575f83247361554fe626953081e8a9e3b8075
yamatakau08 <yamatakau08@gmail.com>
2020-08-20 19:20:10 +0900

    .

--- .tabbar.el
+++ .tabbar.el
@@ -1,8 +1,6 @@
 (use-package tabbar
   :ensure t
-  :config
-  (tabbar-mode 1)
-  (custom-set-variables `(tabbar-buffer-groups-function nil)) ; defvar var is available?
-
+  :init (tabbar-mode 1)
+  :config (custom-set-variables `(tabbar-buffer-groups-function nil)) ; defvar var is available.
   :bind (("C-." . tabbar-forward-tab)))
   
