ff817b0a328d4836424c0f45b68e68b6cc4dadd6
yamatakau08 <yamatakau08@gmail.com>
2020-09-15 17:46:56 +0900

    .

--- .dired-posframe.el
+++ .dired-posframe.el
@@ -3,48 +3,8 @@
   :custom
   (dired-posframe-file-size-limit (* 10 1024 1024))
 
-  :bind (:map dired-mode-map
-	      ("z" . dired-posframe-show)))
-
-;(add-hook 'dired-mode-hook
-;	  (lambda ()
-;	    (define-key dired-mode-map "z" 'dired-posframe-show)))
-
-;(defun peep-dired--image-p (path)
-;  "check if file is a kind of image or text"
-;  ;;(string-match "\\(image\\|text\\)"
-;  (string-match "image"
-;		(shell-command-to-string
-;		 (mapconcat #'shell-quote-argument
-;			    (list "file" path)
-;			    " "))))
-;
-;(defun dired-posframe--show ()
-;  "Show dired-posframe for current dired item."
-;  (let ((path (dired-get-filename nil 'noerror))
-;        hide)
-;    (with-current-buffer (get-buffer-create dired-posframe-buffer)
-;      (let ((inhibit-read-only t))
-;        (erase-buffer)
-;        (cond
-;         ((not path)
-;          (setq hide t))
-;         ((file-directory-p path)
-;          (insert (with-current-buffer (dired-noselect path)
-;                    (buffer-substring-no-properties (point-min) (point-max)))))
-;         ((file-readable-p path)
-;	  (if (peep-dired--image-p path)
-;	      (insert-file-contents path)
-;	    (insert (format "not supported")))
-;
-;          ))
-;        (when path
-;          (let* ((name (file-name-nondirectory path))
-;                 (mode (assoc-default name auto-mode-alist #'string-match)))
-;            (if (memq mode dired-posframe-enable-modes)
-;                (set-auto-mode-0 mode)
-;              (set-auto-mode-0 'fundamental-mode 'keep-mode-if-same))))))
-;    (if hide
-;        (dired-posframe--hide)
-;      (if (peep-dired--image-p path)
-;	  (dired-posframe-display dired-posframe-buffer)))))
+  :bind
+  (:map dired-mode-map
+	("z" . dired-posframe-show) ; don't work,but dired-posframe-show works.
+	)
+  )
--- .thingatpt.el
+++ .thingatpt.el
@@ -1,11 +1,10 @@
 (use-package thingatpt
-  :config
-  ;; add ' ' space, '　' zenkaku space, '\\' to get windows file path
-  ;; if this setting in :custom, Symbol's value as variable is void: thing-at-point-file-name-chars
-  (setq thing-at-point-file-name-chars (concat thing-at-point-file-name-chars " 　\\"))
+  :custom
+  ;; from original value, add ' ' space, '　' zenkaku space, '\\' to get windows file path
+  (thing-at-point-file-name-chars "-~/[:alnum:]_.${}#%,: 　\\")
   )
 
-;thing-at-point-file-name-chars
+;; thing-at-point-file-name-chars
 ;; test file path pattern
 ;; M-: (thing-at-point 'filename)
 ;; file path should be within "file path"
@@ -15,7 +14,6 @@
 ;; fail start with <
 ;; <\\jps00004944\SQA_PostSkips\工数フォルダトップ\FY2019\SQA2_山口　尊広\SQA2_山口　尊広_社員工数メトリクス表_v0080.xlsm>
 
-;;
 ;; on my mac
 ;; "~/Documents/QRチケット印刷 _ スワチケ2入口41段181番.pdf"
 ;: if path has ' ' space or '　' zenkaku space, (things-at-point 'filename) can return path correctly,
