7862df4a9fe26d46b9bdac2915030d6cdf352409
yamatakau08 <yamatakau08@gmail.com>
2020-10-12 23:37:24 +0900

    add tooltip function for the candidates.

--- .ddskk.el
+++ .ddskk.el
@@ -9,4 +9,11 @@
   (skk-jisyo-code 'utf-8) ; refer skk-vars.el
   (skk-jisyo "~/.emacs.d/skk-jisyo/.skk-jisyo")
   (skk-henkan-strict-okuri-precedence t)
+
+  ;; emacs-jp
+  ;; covert candidate will be showin in tooltip, not mini-buffer
+  ;; ddskk use the low level x-show-tip instead of tooltip-show
+  (skk-show-tooltip t)
+  (skk-tooltip-parameters
+   '((foreground-color . "navy blue")))
   )
