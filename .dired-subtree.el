04b4f0ab74110937d00a506afc3a09b519a63e04
yamatakau08 <yamatakau08@gmail.com>
2020-12-08 14:32:49 +0900

    .

--- .dired-subtree.el
+++ .dired-subtree.el
@@ -10,30 +10,14 @@
   :custom-face
   (dired-subtree-depth-1-face
    ((t (:background "snow"))))
-
-  ;; (setq dired-subtree-depth-2-face
-  ;; 	'((t (:background "#232a2b")))
-  ;; 	"Background for depth 2 subtrees"
-  ;; 	:group 'dired-subtree-faces)
-
-  ;; (setq dired-subtree-depth-3-face
-  ;; 	'((t (:background "#212627")))
-  ;; 	"Background for depth 3 subtrees"
-  ;; 	:group 'dired-subtree-faces)
-
-  ;; (setq dired-subtree-depth-4-face
-  ;; 	'((t (:background "#1e2223")))
-  ;; 	"Background for depth 4 subtrees"
-  ;; 	:group 'dired-subtree-faces)
-
-  ;; (setq dired-subtree-depth-5-face
-  ;; 	'((t (:background "#1c1d1e")))
-  ;; 	"Background for depth 5 subtrees"
-  ;; 	:group 'dired-subtree-faces)
-
-  ;; (setq dired-subtree-depth-6-face
-  ;; 	'((t (:background "#1a191a")))
-  ;; 	"Background for depth 6 subtrees"
-  ;; 	:group 'dired-subtree-faces)
-
+  (dired-subtree-depth-2-face
+   ((t (:background "snow"))))
+  (dired-subtree-depth-3-face
+   ((t (:background "snow"))))
+  (dired-subtree-depth-4-face
+   ((t (:background "snow"))))
+  (dired-subtree-depth-5-face
+   ((t (:background "snow"))))
+  (dired-subtree-depth-6-face
+   ((t (:background "snow"))))
   )
