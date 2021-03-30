2db78bdb1a89e0fa3edde2d1fc6fb23bf8c36069
yamatakau08 <yamatakau08@gmail.com>
2021-03-30 15:21:15 +0900

    .

--- .my-plantuml.el
+++ .my-plantuml.el
@@ -1,3 +1,8 @@
 (use-package my-plantuml
-  :load-path "my-plantuml")
-
+  :load-path "my-plantuml"
+  :custom
+  ;;(my-plantuml-java "/C/Program Files (x86)/Common Files/Oracle/Java/javapath/java.exe")
+  ;; Windows 10 /C/Program Files/Java/jdk-12.0.1/bin/java.exe
+  ;; Windows 8  /C/Program Files/Android/Android Studio/jre/bin/java.exe
+  (my-plantuml-java "/C/Program Files/Android/Android Studio/jre/bin/java.exe")
+)
