4060c31fe817a3343c69869a84c412bb1a9e77db
yamatakau08 <yamatakau08@gmail.com>
2021-09-10 22:11:51 +0900

    .

--- lisp/.consult.el
+++ lisp/.consult.el
@@ -66,6 +66,18 @@
 	    (t
 	     (consult-file-externally file-name)))))
 
+  (defun my-consult-dired-grep ()
+    "Search for regexp in files marked dired mode, this works for only mac"
+    ;; https://github.com/minad/consult/issues/407#issuecomment-905342672
+    (interactive)
+    (let ((consult-grep-args
+	   ;;(format "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I %s" ; removed "-r ." and add files by from original
+	   ;;  (mapconcat #'shell-quote-argument (dired-get-marked-files) " ")) ; if marked file is only one file, doesn't work well.
+	   (format   "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I -e ARG OPTS %s"
+		     (mapconcat #'shell-quote-argument (dired-get-marked-files) " ")) ; this works, even if marked file is only one file.
+	   ))
+      (consult-grep)))
+
   ;; :preface Symbol's function definitons is void: consult--grep when M-x consult-ripgrep1 just after launching Emacs, called consult--grep is conslut's internal function from consult-ripgrep1
   ;; revise to call consult-ripgrep
   ;; :demand consult-ripgrep1 is not found when M-x just after launching Emacs
@@ -118,63 +130,6 @@
     (message "%s" (shell-command-to-string cmd))
     ))
 
-;; "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I -r ."
-;; "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I "c:/Temp/SC3/Log/BISYAMON3G-2575/halog_20210820_103646_+0900_fw2.28.0_SC/log/logc20"
-;; (defun consult--grep-sample-builder (input)
-;;   "Build command line given INPUT."
-;;   (pcase-let* ((cmd (split-string-and-unquote consult-grep-args))
-;;                (type (consult--grep-regexp-type (car cmd)))
-;;                (`(,arg . ,opts) (consult--command-split input))
-;;                (`(,re . ,hl) (funcall consult--regexp-compiler arg type)))
-;;     (when re
-;;       (list :command
-;;             (append cmd
-;;                     (list (if (eq type 'pcre) "--perl-regexp" "--extended-regexp")
-;;                           "-e" (consult--join-regexps re type))
-;;                     opts)
-;;             :highlight hl))))
-
-(defun consult-grep-test ()
-  (interactive)
-  (let*
-      ((consult-grep-args "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I ") ; do not search recursively
-       (file "c:/Temp/Log/log1")
-       (dir (file-name-directory file))
-       (file-name (file-name-nondirectory file))
-       (input (format "dummy -- %s" file-name)))
-    (consult-grep dir input)))
-
-;; https://github.com/minad/consult/issues/407#issuecomment-905342672
-;; works only Mac, not Windows
-(defun consult-grep-one-file0 ()
-  "Call `consult-grep' for the current buffer (a single file)."
-  (interactive)
-  (let ((consult-grep-args
-         (concat "grep "
-                 "--line-buffered "
-                 "--color=never "
-                 "--line-number "
-                 "-I "
-                 "-e ARG OPTS "
-                 (shell-quote-argument buffer-file-name))))
-    (message "[consult-grep-one-file] consult-grep-args: %s" consult-grep-args)
-    (consult-grep)))
-
-;; https://github.com/minad/consult/issues/407#issuecomment-906427582
-;; Not work on Windows
-(defun consult-grep-one-file ()
-  "Call `consult-grep' for the current buffer (a single file)."
-  (interactive)
-  (let ((consult-grep-args
-         (concat "grep "
-                 "--line-buffered "
-                 "--color=never "
-                 "--line-number "
-                 "--with-filename "
-                 (shell-quote-argument buffer-file-name))))
-    (message "[consult-grep-one-file] consult-grep-args: %s" consult-grep-args)
-    (consult-grep)))
-
 (defun consult-ripgrep-one-file ()
   "Call `consult-ripgrep' for the current buffer (a single file)."
   (interactive)
@@ -191,57 +146,6 @@
                  (shell-quote-argument buffer-file-name))))
     (consult-ripgrep)))
 
-(defun my-consult-grep-logc-on-mac ()
-  "in testing on Mac works"
-  (interactive)
-  (let* ((file "/Users/yama/bin/ptools/mylogcat/SC3/Log/BISYAMON3G-2575/var/logc1") ; 1
-	 ;;(file "~/bin/ptools/mylogcat/SC3/Log/BISYAMON3G-2575/var/logc1") ; 2
-	 ;;(consult-grep-args (format "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I %s" file)) ; 1,2 fail, need "-e ARG OPTS" as option
-
-	 (consult-grep-args (format "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I -e ARG OPTS %s" (expand-file-name file))) ;; 1 pass
-	 )
-    (consult-grep)))
-
-;; (defun consult--grep (prompt builder dir initial)
-;;   "Run grep in DIR.
-
-;; BUILDER is the command builder.
-;; PROMPT is the prompt string.
-;; INITIAL is inital input."
-;;   (let* ((prompt-dir (consult--directory-prompt prompt dir))
-;;          (default-directory (cdr prompt-dir))
-;;          (read-process-output-max (max read-process-output-max (* 1024 1024))))
-;;     (consult--read
-;;      (consult--async-command builder
-;;        (consult--grep-format builder)
-;;        :file-handler t) ;; allow tramp
-;;      :prompt (car prompt-dir)
-;;      :lookup #'consult--lookup-member
-;;      :state (consult--grep-state)
-;;      :initial (consult--async-split-initial initial)
-;;      :add-history
-;;      (when-let (thing (thing-at-point 'symbol))
-;;        (consult--async-split-initial thing))
-;;      :require-match t
-;;      :category 'consult-grep
-;;      :group #'consult--grep-group
-;;      :history '(:input consult--grep-history)
-;;      :sort nil)))
-
-(defun consult-grep-specified-files ()
-  (interactive)
-  (let ((consult-grep-args
-	 (concat "grep --line-buffered --color=never --ignore-case --exclude-dir=.git --line-number -I " ; removed "-r ." by from original
-		 ;;(mapconcat #'shell-quote-argument (dired-get-marked-files) " "))))
-		 (mapconcat #'shell-quote-argument '("/tmp/Log/log1" "/tmp/Log/log2" "/tmp/Log/log3") " "))))
-    (consult-grep nil "-- --invert-match")))
-
-(defun consult-ripgrep-invert-match (&optional dir initial)
-  (interactive "P")
-  (let ((consult-ripgrep-args "rg --hidden --invert-match --line-buffered --color=never --max-columns=1000 --path-separator /\
-   --smart-case --no-heading --line-number ."))
-    (consult--grep "Ripgrep" #'consult--ripgrep-builder dir initial)))
-
 (defun consult-ripgrep-work-on-mac ()
   "Call `consult-ripgrep' for the multiple file works on Mac, not on Windows"
   (interactive)
@@ -255,32 +159,22 @@
                  "--max-columns=250 "
                  "--max-columns-preview "
                  "--with-filename "
+                 ;;(shell-quote-argument "/User/yama/tmp/log1")
                  (shell-quote-argument "/tmp/log1")
 		 " "
+                 ;;(shell-quote-argument "/User/yama/tmp/log2"))))
                  (shell-quote-argument "/tmp/log2"))))
     (consult-ripgrep)))
 
-;; in testing
-(defun my-consult--ripgrep-builder (input)
-  "Build command line given INPUT."
-  (pcase-let* ((cmd (split-string-and-unquote consult-ripgrep-args))
-               (type (consult--ripgrep-regexp-type (car cmd)))
-               (`(,arg . ,opts) (consult--command-split input))
-               (`(,re . ,hl) (funcall consult--regexp-compiler arg type)))
-    (when re
-      (let ((ret (list :command
-		       (append cmd
-			       (and (eq type 'pcre) '("-P"))
-			       (list  "-e" (consult--join-regexps re type))
-			       (shell-quote-argument "c:/Temp/SC3/Log/BISYAMON3G-2575/halog_20210820_103646_+0900_fw2.28.0_SC/log/logc1")
-			       opts
-			       )
-		       :highlight hl)))
-	(message "%s" ret)
-	ret))))
-
-(defun consult-ripgrep-tako (&optional dir initial)
-  (interactive "P")
-  (consult--grep "Ripgrep" #'my-consult--ripgrep-builder dir initial))
+(defun my-consult-dired-ripgrep ()
+    "Search for regexp in files marked dired mode, this works for only mac"
+    ;; https://github.com/minad/consult/issues/407#issuecomment-905342672
+    (interactive)
+    (let ((consult-ripgrep-args
+	   ;;(format "rg --line-buffered --color=never --max-columns=1000 --path-separator --smart-case --no-heading --line-number --with-filename "
+	   (format "rg --line-buffered --color=never --line-number --smart-case --no-heading --max-columns=250 --max-columns-preview --with-filename "
+		   (mapconcat #'shell-quote-argument (dired-get-marked-files) " "))
+	   ))
+      (consult-ripgrep)))
 
 (provide '.consult)
