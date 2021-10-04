92f5cc1564939980cdd4f35491dc517d2f45c501
yamatakau08 <yamatakau08@gmail.com>
2021-10-04 13:51:54 +0900

    .

--- lisp/.bookmark+.el
+++ lisp/.bookmark+.el
@@ -7,24 +7,19 @@
 	("C-j" . my-bmkp-jump))
 
   :config
-  (defun my-bmkp-jump-file-externally (bookmark-entry)
-    "this is for handler in bookmark file, refer consult-file-externally"
-    (interactive)
-    (let ((file-name (let-alist bookmark-entry .filename)))
-      ;;(w32-shell-execute "open" (expand-file-name file-name)))) ; no need to expand-file-name
-      (w32-shell-execute "open" file-name)))
-
   (defun my-bmkp-jump ()
     "open the bookmark with Windows explore or application associated the file suffix"
     (interactive)
     (let* ((bookmark (bookmark-bmenu-bookmark))
 	   (filename (bookmark-get-filename bookmark)))
-      ;;(bmkp-jump-1 bookmark #'my-bmkp-jump-display-func)
+      ;;(bmkp-jump-1 bookmark #'my-bmkp-jump-display-func) ; need to consider to use bmkp-jump-1
       (cond ((eq (window-system) 'w32)
-	     (cond ((file-directory-p filename)
+	     (cond ((eq (bookmark-get-handler bookmark) #'bmkp-jump-url-browse)
+		    (bmkp-jump-url-browse bookmark))
+		   ((file-directory-p filename)
 		    (w32-shell-execute "explore" filename "/e,/select,"))
 		   ((string= (file-name-extension filename) "mp4")
-		    ;;(w32-shell-execute "start" filename)) ; for Windows 8.1
+		    ;;(w32-shell-execute "open" filename)) ; not available on  Windows 8.1
 		    (shell-command-to-string (format "%s %s" "start" filename)))
 		   (t
 		    (w32-shell-execute "open" filename))))
