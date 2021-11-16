31a444625306c12802afe1b6018783bcb217ea94
yamatakau08 <yamatakau08@gmail.com>
2021-11-16 23:03:48 +0900

    .

--- lisp/.url-vars.el
+++ lisp/.url-vars.el
@@ -1,6 +1,5 @@
 ;;; to avoid the following error message in company network on Emacs 27.0
 ;;; in case, M-x pakage-refresh-contents
-;;; https://emacs-jp.slack.com/archives/C1B5WTJLQ/p1547097797720600
 ;;; Importing package-keyring.gpg...done
 ;;; error in process sentinel: if: Error retrieving: http://melpa.org/packages/archive-contents (error connection-failed "failed with code 110" :host "melpa.org" :service 80)
 ;; (if (eq (my-network-type) 'company)
@@ -9,12 +8,19 @@
 ;; 	    `(("http"  . ,proxy-server-port)
 ;; 	      ("https" . ,proxy-server-port)))))
 
-(use-package url-vars
-  :custom
-  (url-proxy-services `(("http" . ,(when (eq (my-network-type) 'company)
-				     (format "%s:%s" wl-proxy-server wl-proxy-port)))
+;; (use-package url-vars
+;;   :custom
+;;   (url-proxy-services `(("http"  . ,(when (eq (my-network-type) 'company)
+;; 				      (format "%s:%s" wl-proxy-server wl-proxy-port)))
+;; 			("https" . ,(when (eq (my-network-type) 'company)
+;; 				      (format "%s:%s" wl-proxy-server wl-proxy-port)))))
+;; )
+					;
+;; don't use use-package, because my emacs 27.0.50 on Linux has Bad request
+(custom-set-variables
+ '(url-proxy-services `(("http"  . ,(when (eq (my-network-type) 'company)
+				      (format "%s:%s" wl-proxy-server wl-proxy-port)))
 			("https" . ,(when (eq (my-network-type) 'company)
-				     (format "%s:%s" wl-proxy-server wl-proxy-port)))))
-  )
+				      (format "%s:%s" wl-proxy-server wl-proxy-port))))))
 
 (provide '.url-vars)
