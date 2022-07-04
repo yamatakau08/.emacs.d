4175e951b6159a532a82c1303a6732f970e2c8d0
yamatakau08 <yamatakau08@gmail.com>
2022-07-04 13:30:52 +0900

    .

--- lisp/.my-plantuml.el
+++ lisp/.my-plantuml.el
@@ -9,7 +9,8 @@
 
   ;; Cygwin
   (my-plantuml-java "c:/Program Files/Android/Android Studio/jre/bin/java.exe") ; availabe windows drive letter notation
-  (my-plantuml-plantuml "c:/Program Files/PlantUML/plantuml-1.2022.3.jar") ; availabe windows drive letter notation
+  ;;(my-plantuml-plantuml "c:/Program Files/PlantUML/plantuml-1.2022.3.jar") ; availabe windows drive letter notation
+  (my-plantuml-plantuml "c:/yama/bin/plantuml.jar") ; availabe windows drive letter notation on Laptop
 
   )
 
