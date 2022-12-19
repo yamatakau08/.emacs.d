54a77e93455a78ecbaffcaf84955eded175b2f15
yamatakau08 <yamatakau08@gmail.com>
2022-12-19 14:09:07 +0900

    add the function "helm-anki-browse-english"

--- lisp/.helm-anki-browse.el
+++ lisp/.helm-anki-browse.el
@@ -1,6 +1,12 @@
 (use-package helm-anki-browse
   :load-path "my-anki-browse"
-  :after my-anki-browse)
+  :after my-anki-browse
+
+  :config
+  (defun helm-anki-browse-english ()
+    (interactive)
+    (helm-anki-browse "英語"))
+  )
 
 (provide '.helm-anki-browse)
 
