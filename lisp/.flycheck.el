bf99a5f13c79054ed5b9e545f9476650c67abac0
yamatakau08 <yamatakau08@gmail.com>
2023-01-24 14:37:16 +0900

    disabled flycheck on Windows environment, flycheck makes the editing too slow.

--- lisp/.flycheck.el
+++ lisp/.flycheck.el
@@ -1,7 +1,7 @@
 (use-package flycheck
   :ensure t
+  :unless (eq (window-system) 'w32) ; on Windows environment, flycheck makes the editing too slow.
   :hook (ruby-mode . flycheck-mode)
   )
 
 (provide '.flycheck)
-
