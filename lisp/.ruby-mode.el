218c364cc83789c94ed71ab0fcb9d0a8328812bd
yamatakau08 <yamatakau08@gmail.com>
2023-07-20 14:42:27 +0900

    enable C-M-d delete-trailing-whitespace in ruby-mode

--- lisp/.ruby-mode.el
+++ lisp/.ruby-mode.el
@@ -1,6 +1,11 @@
 (use-package ruby-mode
   :custom
   ;; to supress "#coding: utf-8" is automatically inserted at top of the ruby script after saving
-  (ruby-insert-encoding-magic-comment nil))
+  (ruby-insert-encoding-magic-comment nil)
+
+  :bind
+  (:map ruby-mode-map
+	("C-M-d" . delete-trailing-whitespace)) ; originally assinged smie-down-list in ruby-mode-map
+  )
 
 (provide '.ruby-mode)
