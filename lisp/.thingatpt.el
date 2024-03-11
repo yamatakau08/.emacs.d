81e0cbb5aa26b6bfe06017f6a96342bb3211e60b
yamatakau08 <yamatakau08@gmail.com>
2024-03-11 14:57:26 +0900

    delete the unused function and test patterns.

--- lisp/.thingatpt.el
+++ lisp/.thingatpt.el
@@ -5,16 +5,6 @@
   ;; When the variable is set by defvar function, should use setq function
   (setq thing-at-point-file-name-chars "-~/[:alnum:]_.${}#%,: 　\\")
 
-  (defun xmy-thing-at-point-filename ()
-    "Even if cursor is on '\"' or '<' character is preceding file name, returns filename"
-    (let* ((byte (get-byte))
-	   (char (format "%c" byte))) ; alternative (char (string (char-after)))
-      (save-excursion
-	(if (or (equal char "\"")
-		(equal char "<"))
-	    (forward-char))
-	(thing-at-point 'filename t))))
-
   (defun my-thing-at-point-filename ()
     "Even if cursor is on '\"' or '<' character is preceding file name, returns filename"
     (save-excursion
@@ -22,16 +12,6 @@
       (thing-at-point 'filename t)))
 
   )
-
-;; thing-at-point-file-name-chars
-;; test file path pattern
-;; M-: (thing-at-point 'filename)
-;; file path should be within "file path"
-;; on windows
-;; "\\jps00004944\share_data\SQA1-HA\10_Test_Project_FY17\NewAudio\02_検証計画書\Schedule-NewAudio-Evaluation.pptx"
-;; "\\jps00004944\SQA_PostSkips\工数フォルダトップ\FY2019\SQA2_山口　尊広\SQA2_山口　尊広_社員工数メトリクス表_v0080.xlsm"
-;; fail when point is on '<'
-;; <\\jps00004944\SQA_PostSkips\工数フォルダトップ\FY2019\SQA2_山口　尊広\SQA2_山口　尊広_社員工数メトリクス表_v0080.xlsm>
 
 ;; on my mac
 ;; "~/Documents/QRチケット印刷 _ スワチケ2入口41段181番.pdf"
