59ab98d522ab6c0049add996a3ea4d8a77914068
yamatakau08 <yamatakau08@gmail.com>
2024-03-13 14:40:58 +0900

    enable all the icons.

--- lisp/.all-the-icons-dired.el
+++ lisp/.all-the-icons-dired.el
@@ -1,5 +1,8 @@
 (use-package all-the-icons-dired
   :ensure t
+
+  :after (all-the-icons)
+
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (provide '.all-the-icons-dired)
--- lisp/.all-the-icons.el
+++ lisp/.all-the-icons.el
@@ -1,5 +1,25 @@
+;;; You need to install fonts used by all-the-icons package
+;;; git clone git@github.com:domtronn/all-the-icons.el.git
+;;; go to the fonts directory under the git clone directory
+;;; do dobule click the ttf font files to install that into fonts system
+;;; the following lisp expression can judge if the fonts is installed.
+
+;; (member "all-the-icons" (font-family-list))
+;; (member "FontAwesome" (font-family-list))
+;; (member "file-icons" (font-family-list))
+;; (member "Material Icons" (font-family-list))
+;; (member "github-octicons" (font-family-list))
+;; (member "Weather Icons" (font-family-list))
+
 (use-package all-the-icons
   :ensure t
+
+  :if (and (member "all-the-icons" (font-family-list))
+	   (member "FontAwesome" (font-family-list))
+	   (member "file-icons" (font-family-list))
+	   (member "Material Icons" (font-family-list))
+	   (member "github-octicons" (font-family-list))
+	   (member "Weather Icons" (font-family-list)))
 
   :config
   ;; https://github.com/domtronn/all-the-icons.el#slow-rendering
@@ -7,4 +27,3 @@
   (setq inhibit-compacting-font-caches t))
 
 (provide '.all-the-icons)
-
