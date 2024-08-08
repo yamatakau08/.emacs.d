dc330a5f6d3730ce2980018d463f0a46746a296c
yamatakau08 <yamatakau08@gmail.com>
2024-08-08 09:07:06 +0900

    .

---
+++ lisp/.ellama.el
@@ -0,0 +1,12 @@
+(use-package ellama
+  :ensure t
+
+  :init
+  ;; setup key bindings
+  ;;(setopt ellama-keymap-prefix "C-c e")
+  ;; language you want ellama to translate to
+  (setopt ellama-language "Japanese")
+  )
+
+(provide '.ellama)
+
