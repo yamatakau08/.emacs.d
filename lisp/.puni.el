f13a6bf959c15855dcf84edc17715bf0357994a1
yamatkau08 <yamatakau08@gmail.com>
2025-09-28 14:06:04 +0900

    add key bind for puni-splice and puni-squeeze in puni-mode-map

--- lisp/.puni.el
+++ lisp/.puni.el
@@ -1,13 +1,20 @@
 ;; https://github.com/AmaiKinono/puni?tab=readme-ov-file#quick-start
 (use-package puni
   :ensure t
-  
+
   :defer t
   :init
   ;; The autoloads of Puni are set up so you can enable `puni-mode` or
   ;; `puni-global-mode` before `puni` is actually loaded. Only after you press
   ;; any key that calls Puni commands, it's loaded.
   (puni-global-mode)
-  (add-hook 'term-mode-hook #'puni-disable-puni-mode))
+  (add-hook 'term-mode-hook #'puni-disable-puni-mode)
+
+  :bind
+  (:map puni-mode-map
+	("C-c s" . puni-splice)
+	("C-c z" . puni-squeeze))
+
+  )
 
 (provide '.puni)
