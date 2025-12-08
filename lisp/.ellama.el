b60c4392ee410d5783d48d00a8a009954ca3a06a
yamatakau08 <yamatakau08@gmail.com>
2025-12-08 22:10:12 +0900

    set model to "gemma3:latest"

--- lisp/.ellama.el
+++ lisp/.ellama.el
@@ -6,6 +6,13 @@
   ;;(setopt ellama-keymap-prefix "C-c e")
   ;; language you want ellama to translate to
   (setopt ellama-language "Japanese")
+  (require 'llm-ollama)
+  ;; normal conversation
+  (setopt ellama-provider
+	  (make-llm-ollama
+	  :chat-model "gemma3:latest" ; gpt-oss-20b is too slow
+	  :default-chat-non-standard-params '(("num_ctx" . 8192))))
+  ;; for coding
   )
 
 (provide '.ellama)
