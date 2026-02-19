a6b499e26089c26ee9df639d01a2015784c8aa5b
yamatakau08 <yamatakau08@gmail.com>
2026-02-20 04:29:16 +0900

    suppress auto-insert mode when ellama new session starts

--- lisp/.ellama.el
+++ lisp/.ellama.el
@@ -14,25 +14,25 @@
 	   :default-chat-non-standard-params '(("num_ctx" . 8192))))
   (setopt ellama-providers
 	  '(;; "gemini-3-pro-preview" ; not available in the free tier
-	    ;; ("gemini-3-pro-preview" . (make-llm-gemini
-	    ;; 			       :key (auth-source-pick-first-password :user "gen-lang-client-0425588602")
-	    ;; 			       :chat-model "gemini-3-pro-preview"))
 	    ("gemini-3-flash-preview" . (make-llm-gemini
 					 :key (auth-source-pick-first-password :user "gen-lang-client-0425588602")
 					 :chat-model "gemini-3-flash-preview"))
+	    ;; "gemini-2.5-pro" ; not available in the free tier
 	    ("gemini-2.5-flash" . (make-llm-gemini
 				   :key (auth-source-pick-first-password :user "gen-lang-client-0425588602")
 				   :chat-model "gemini-2.5-flash"))
 	    ("gemini-2.5-flash-lite" . (make-llm-gemini
 				   :key (auth-source-pick-first-password :user "gen-lang-client-0425588602")
 				   :chat-model "gemini-2.5-flash-lite"))
-	    ;; "gemini-3-pro-preview" ; not available in the free tier
-	    ;; ("gemini-2.5-pro" . (make-llm-gemini
-	    ;; 			 :key (auth-source-pick-first-password :user "gen-lang-client-0425588602")
-	    ;; 			 :chat-model "gemini-2.5-pro"))
 	    ))
 
   :config
+  ;; to suppress auto-insert mode
+  (advice-add 'ellama-new-session :around
+              (lambda (orig-fun &rest args)
+                (let ((auto-insert nil))
+                  (apply orig-fun args))))
+
   (defun my-ellama-generate-commit-message ()
     "Show the commit message genereated by ellama-generate-commit-message in the buffer *ellama-commit-message*"
     (interactive)
