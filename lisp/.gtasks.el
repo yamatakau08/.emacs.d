95cd0e19de396e913af748565a62ee376bc7932c
yamatakau08 <yamatakau08@gmail.com>
2026-03-16 15:59:33 +0900

    Moved the `gtasks-client-id` and `gtasks-client-secret` definitions from the `use-package`'s `:custom` block to the `:config` block.

--- lisp/.gtasks.el
+++ lisp/.gtasks.el
@@ -1,12 +1,11 @@
 (use-package gtasks
   :ensure t
 
-  :custom
-  (gtasks-client-id (plist-get (car (auth-source-search :host "gtasks")) :client-id))
-  (gtasks-client-secret (auth-source-pick-first-password :host "gtasks"))
-
   :config
   (setq gtasks--redirect-uri "http://localhost")
+
+  (setopt gtasks-client-id (plist-get (car (auth-source-search :host "gtasks")) :client-id))
+  (setopt gtasks-client-secret (auth-source-pick-first-password :host "gtasks"))
 
   )
 
